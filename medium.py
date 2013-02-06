a1f91457cfdab44c1d04bf65f87cb7d5648de3de
Eleonore9 <eleonore.mayola@ymail.com>
2013-02-06 08:30:01 -0800

    added second html template (intro)

--- medium.py
+++ medium.py
@@ -2,54 +2,53 @@
 # -*- coding=UTF-8 -*-
 import os, sys
 from flask import Flask, render_template, redirect, request, url_for, session, g
-import model
+from dates.format import timesince
+
 app = Flask(__name__)
 
 
 # app.py template from the Tipsy tutorial
 
+#-------- NO NEED CAUSE NO DATABASE! ------------------------
+# #connect to db before any request and disconnect afterwards
+# @app.before_request
+# def set_up_db():
+#     g.db = model.connect_db()
 
-#connect to db before any request and disconnect afterwards
-@app.before_request
-def set_up_db():
-    g.db = model.connect_db()
-
-@app.teardown_request
-def disconnect_db(e):
-    g.db.close()
-#------------------
+# @app.teardown_request
+# def disconnect_db(e):
+#     g.db.close()
+# #------------------
 
 
-# go to index and login views
+#-------- GO TO INDEX -------------------------------------- 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
-def login():
-    return render_template("login.html")
+#-------- NO LOGIN NEEDED ----------------------------------
+# @app.route("/login")
+# def login():
+#     return render_template("login.html")
 #---------------------------------------
+# #authentification of the user and opening the session:
+# @app.route("/authenticate", methods=['POST'])
+# def authenticate():
+#     email = request.form['email']
+#     password = request.form['password']
+#     user = model.User.authenticate(g.db, email, password)
+#     session['user_id'] = user.id
+#     # flash('You were successfully logged in!')
+#     return redirect(url_for("list_tasks"))
+# #-----------------------------------------
 
-
-#authentification of the user and opening the session:
-@app.route("/authenticate", methods=['POST'])
-def authenticate():
-    email = request.form['email']
-    password = request.form['password']
-    user = model.User.authenticate(g.db, email, password)
-    session['user_id'] = user.id
-    # flash('You were successfully logged in!')
-    return redirect(url_for("list_tasks"))
-#-----------------------------------------
-
-
-#playing with the tasks:
-@app.route("/tasks")
-def list_tasks():
-    user_id = session.get("user_id", None)
-    user_obj = model.User.get_user(g.db, user_id)
-    tasks_from_db = user_obj.get_tasks(g.db, user_id)
-    return render_template("list_tasks.html", tasks=tasks_from_db)
+#-------- HELLO MESSAGE AND INTRO -------------------------
+@app.route("/intro", methods=["POST"])
+def intro():
+    name = request.form['name']
+    environment = jinja2.environment(whatever)
+    environment.filters['timesince'] = timesince
+    return render_template("intro.html", name=name)
 
 @app.route("/save_task", methods=["POST"])
 def save_task():
@@ -71,15 +70,15 @@ def complete_task(id):
     task = model.Task.get_task(g.db, id)
     task.complete_task(g.db, id)
     return redirect(url_for("list_tasks"))
-#-----------------------------------------
+#---------------------------------------------------------
 
-
-#logout = close the session
-@app.route("/logout")
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for("index"))
-#------------------------------------
+#------- NOT NEEDED --------------------------------------
+# #logout = close the session
+# @app.route("/logout")
+# def logout():
+#     session.pop('user_id', None)
+#     return redirect(url_for("index"))
+# #------------------------------------
 
 
 
