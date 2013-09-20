c69c26b6aa38ee2dfc9c4a1a3c33736ecdf13411
Eleonore9 <eleonore.mayola@ymail.com>
2013-09-20 12:26:53 +0200

    remove tests from master branch

--- medium_app.py
+++ medium_app.py
@@ -2,22 +2,11 @@
 # -*- coding=UTF-8 -*-
 import os, sys
 from flask import Flask, render_template, url_for
-from flask.ext.jasmine import Jasmine
 
 #port=int(os.environ.get('PORT', 33507))
 #heroku config:add PORT=33507
 
 app = Flask(__name__)
-
-jasmine = Jasmine(app)
-
-jasmine.specs(
-    './tests/spec/mediumSpec.js',
-)
-
-jasmine.sources(
-    './static/js/medium.js'
-)
 
 
 #-------- GO TO APP --------------------------------------
